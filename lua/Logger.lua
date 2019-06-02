b7b31814557e52f968bd38fa8829f7b2334dcffe
Vincent Saulue-Laborde <vincent.saulue@hotmail.fr>
2019-06-02 12:20:01 +0200

    Logger: add warn function & comments.

--- lua/Logger.lua
+++ lua/Logger.lua
@@ -19,22 +19,52 @@
 local Logger = {
     debug = nil, -- implemented later
     error = nil, -- implemented later
+    warn = nil, -- implemented later
 }
 
+-- Implementation stuff (private scope).
 local Impl = {
     -- Prefix for all messages.
     Prefix = "[" .. script.mod_name .. "] "
 }
 
 -- Logs a debug message.
+--
+-- A debug message is just a trace message to help the developers understand what's happening.
+-- They should not be displayed anywhere in a "production" environment.
+--
+-- Args:
+-- * message: Message to log.
+--
 function Logger.debug(message)
     game.print(Impl.Prefix .. "Debug: " .. message)
 end
 
 -- Logs an error message.
+--
+-- An error message is raised when the mod is in an inconsistent state.
+--
+-- The mod is now in "undefined behaviour" mode: it is extremely likely to give wrong results
+-- from now.
+--
+-- Args:
+-- * message: Message to log.
+--
 function Logger.error(message)
     game.print(Impl.Prefix .. "Error: " .. message)
     game.print(debug.traceback())
+end
+
+-- Logs a warning message.
+--
+-- A warning message is raised when the mod reads data from Factorio that looks wrong. The mod will try to recover
+-- from this state (by making guesses, or ignoring the data).
+--
+-- Args:
+-- * message: Message to log.
+--
+function Logger.warn(message)
+    game.print(Impl.Prefix .. "Warning: " .. message)
 end
 
 return Logger
