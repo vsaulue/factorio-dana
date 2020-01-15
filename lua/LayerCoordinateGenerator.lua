68f1bc275f52350f024afee2615ff8c395802e5b
Vincent Saulue-Laborde <vincent.saulue@hotmail.fr>
2020-01-15 13:49:54 +0100

    LayerCoordinateGenerator: attach link tree nodes to entry slots.

--- lua/LayerCoordinateGenerator.lua
+++ lua/LayerCoordinateGenerator.lua
@@ -15,9 +15,9 @@
 -- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
 
 local ErrorOnInvalidRead = require("lua/ErrorOnInvalidRead")
+local LayerTreeLinkGenerator = require("lua/LayerTreeLinkGenerator")
 local LayoutCoordinates = require("lua/LayoutCoordinates")
 local Logger = require("lua/Logger")
-local Tree = require("lua/Tree")
 
 -- Helper class to compute the final coordinates of a LayerLayout object.
 --
@@ -31,15 +31,6 @@ local Tree = require("lua/Tree")
 --
 local LayerCoordinateGenerator = ErrorOnInvalidRead.new{
     run = nil, -- implemented later
-}
-
--- Implementation stuff (private scope).
-local Impl = ErrorOnInvalidRead.new{
-    getMiddleEntryPos = nil, -- implemented later
-
-    buildTree = nil, -- implemented later
-
-    buildTreeImpl = nil, -- implemented later
 }
 
 -- Computes the coordinates of each elements of a LayerLayout object.
@@ -103,112 +94,9 @@ function LayerCoordinateGenerator.run(layout, params)
     end
     for _,pos in pairs(layout.layers.reverse.vertex) do
         local vertexEntry = layout.layers.entries[pos[1]][pos[2]]
-        Impl.buildTree(vertexEntry, layout.layers.links.forward, self.entryPositions, result.links)
-        Impl.buildTree(vertexEntry, layout.layers.links.backward, self.entryPositions, result.links)
+        LayerTreeLinkGenerator.run(self, vertexEntry)
     end
     return result
-end
-
--- Gets the coordinates of the middle of an entry.
---
--- Args:
--- * entry: Entry which coordinates will be computed.
--- * entryPositions[type,index]: map of entries position data.
---
--- Returns: x,y coordinates of the middle of the entry.
---
-function Impl.getMiddleEntryPos(entry, entryPositions)
-    local entryPos = entryPositions[entry.type][entry.index]
-    local entryMiddleX = (entryPos.xMin + entryPos.xMax) / 2
-    local entryMiddleY = (entryPos.yMin + entryPos.yMax) / 2
-    return entryMiddleX, entryMiddleY
-end
-
--- Implementation of Impl.buildTree.
---
--- TODO: non-recursive implementation
---
--- Args:
--- * entry: Root of the tree being built.
--- * links: Map of links to use (ex: Layers.link.forward or Layers.links.backward).
--- * entryPositions[type,index]: Map holding the position of each entry.
---
--- Returns: A tree of the links from entry.
---
-function Impl.buildTreeImpl(entry,links, entryPositions)
-    local entryX, entryY = Impl.getMiddleEntryPos(entry, entryPositions)
-    local result = Tree.new({
-        x = entryX,
-        y = entryY,
-    })
-    if entry.type == "linkNode" then
-        for link in pairs(links[entry]) do
-            local otherEntry = link:getOtherEntry(entry)
-            local subtree = Impl.buildTreeImpl(otherEntry, links, entryPositions)
-            result.children[subtree] = true
-        end
-    else
-        if entry.type ~= "edge" then
-            Logger.error("LayerLayout: link connecting multiple vertices together.")
-        end
-        result.type = entry.type
-        result.index = entry.index
-    end
-    return result
-end
-
--- Builds trees for each link starting from the specified vertex entry.
---
--- This function builds a tree of links starting from the given vertex entry. It
--- is only allowed to cross "linkNode" entries.
---
--- Args:
--- * entry: Vertex entry from which to start.
--- * links: Map of links to use (ex: Layers.link.forward or Layers.links.backward).
--- * entryPositions[type,index]: Map holding the position of each entry.
--- * output: The table in which to store the generated trees.
---
-function Impl.buildTree(entry, links, entryPositions, output)
-    local forwardCount = 0
-    local entryX, entryY = Impl.getMiddleEntryPos(entry, entryPositions)
-    local forwardTree = Tree.new({
-        type = entry.type,
-        index = entry.index,
-        x = entryX,
-        y = entryY,
-    })
-    local backwardCount = 0
-    local backwardTree = Tree.new({
-        type = entry.type,
-        index = entry.index,
-        x = entryX,
-        y = entryY,
-    })
-    for link in pairs(links[entry]) do
-        local otherEntry = link:getOtherEntry(entry)
-        local newTree = Impl.buildTreeImpl(otherEntry, links, entryPositions)
-        if link.channelIndex.isForward then
-            forwardTree.children[newTree] = true
-            forwardCount = forwardCount + 1
-        else
-            backwardTree.children[newTree] = true
-            backwardCount = backwardCount + 1
-        end
-    end
-    if forwardCount > 0 then
-        local newTreeLink = ErrorOnInvalidRead.new{
-            tree = forwardTree,
-            category = "forward",
-        }
-        output[newTreeLink] = true
-    end
-    if backwardCount > 0 then
-        local newTreeLink = ErrorOnInvalidRead.new{
-            tree = backwardTree,
-            category = "backward",
-        }
-        output[newTreeLink] = true
-    end
 end
 
 return LayerCoordinateGenerator
