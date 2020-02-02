4a301682c26c4455a4b4cbffb72c533f1b7ea2a1
Vincent Saulue-Laborde <vincent.saulue@hotmail.fr>
2020-02-02 12:20:31 +0100

    LayerCoordinateGenerator: switch to "channel routing" representation of links.

--- lua/LayerCoordinateGenerator.lua
+++ lua/LayerCoordinateGenerator.lua
@@ -14,8 +14,8 @@
 -- You should have received a copy of the GNU General Public License
 -- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
 
+local ChannelTreeBuilder = require("lua/ChannelTreeBuilder")
 local ErrorOnInvalidRead = require("lua/ErrorOnInvalidRead")
-local LayerTreeLinkGenerator = require("lua/LayerTreeLinkGenerator")
 local LayoutCoordinates = require("lua/LayoutCoordinates")
 local Logger = require("lua/Logger")
 
@@ -24,14 +24,55 @@ local Logger = require("lua/Logger")
 -- This class is private (not accessible from other files): it is just an intermediate
 -- used during the layout generation.
 --
+-- Subtypes:
+-- * LayerEntryPosition: Class holding placement data of a specific entry.
+--   * output: placement data of this entry, returned in the LayoutCoordinates object.
+--   * inboundNodes[channelIndex]: Tree node of the given channel index for inbound slots.
+--   * outboundNodes[channelIndex]: Tree node of the given channel index for outbound slots.
+--
 -- Fields:
--- * entryPositions[type][index]: Placement data of the entry with the given type & index.
+-- * entryPositions[entry]: LayerEntryPosition object associated to a specific entry.
 -- * layout: Input LayerLayout instance.
+-- * params: LayoutParameters object, describing constraints for the coordinates of elements.
 -- * result: Output LayoutCoordinates object.
 --
 local LayerCoordinateGenerator = ErrorOnInvalidRead.new{
     run = nil, -- implemented later
 }
+
+-- Creates a tree links for all the channel indices of a channel layers.
+--
+-- All trees will be stored in self.result.links. The nodes attached to an entry will be properly
+-- added to the corresponding LayerEntryPosition objects inside self.entryPositions. The Y
+-- field of these nodes are NOT set by this function.
+--
+-- Args:
+-- * self: LayerCoordinateGenerator object being build.
+-- * lRank: Rank of the channel layer to build.
+-- * yMin: maximum "y" coordinate of objects in the last layer.
+--
+-- Returns: The "y" coordinate from which the next layer should be placed.
+--
+local function processChannelLayer(self, lRank, yMin)
+    local channelLayer = self.layout.channelLayers[lRank]
+    local y = yMin
+    for cRank=1,channelLayer.order.count do
+        local channelIndex = channelLayer.order[cRank]
+        local channelTree = null
+        y = y + self.params.linkWidth
+        local newTree = ChannelTreeBuilder.run(channelLayer, channelIndex, self.entryPositions, y)
+        local category = "forward"
+        if not channelIndex.isForward then
+            category = "backward"
+        end
+        local treeLink = ErrorOnInvalidRead.new{
+            category = category,
+            tree = newTree,
+        }
+        self.result.links[treeLink] = true
+    end
+    return y + self.params.linkWidth
+end
 
 -- Computes the coordinates of each elements of a LayerLayout object.
 --
@@ -50,6 +91,7 @@ function LayerCoordinateGenerator.run(layout, params)
             vertex = result.vertices,
         },
         layout = layout,
+        params = params,
         result = result,
     }
     local typeToMinX = ErrorOnInvalidRead.new{
@@ -64,14 +106,13 @@ function LayerCoordinateGenerator.run(layout, params)
     }
     local typeToMinY = ErrorOnInvalidRead.new{
         edge = params.edgeMinY,
-        linkNode = math.max(params.edgeMinY, params.vertexMinY),
+        linkNode = 0,
         vertex = params.vertexMinY,
     }
     local yLayerLength = math.max(
         params.edgeMinY + 2 * params.edgeMarginY,
         params.vertexMinY + 2 * params.vertexMarginY
     )
-    local middleY = yLayerLength / 2
     for layerId=1,layout.layers.entries.count do
         local layer = layout.layers.entries[layerId]
         local x = 0
@@ -82,21 +123,58 @@ function LayerCoordinateGenerator.run(layout, params)
             local xLength = math.max(typeToMinX[entryType], params.linkWidth * maxSlotsCount)
             local xMargin = typeToMarginX[entryType]
             x = x + xMargin
-            local yHalfLength = typeToMinY[entryType] / 2
-            self.entryPositions[entry.type][entry.index] = ErrorOnInvalidRead.new{
-                xMin = x,
-                xMax = x + xLength,
-                yMin = middleY - yHalfLength,
-                yMax = middleY + yHalfLength,
+            local entryRecord = ErrorOnInvalidRead.new{
+                output = ErrorOnInvalidRead.new{
+                    xMin = x,
+                    xMax = x + xLength,
+                },
+                inboundNodes = ErrorOnInvalidRead.new(),
+                outboundNodes = ErrorOnInvalidRead.new(),
             }
+            if entryType ~= "linkNode" then
+                local tableName = "vertices"
+                if entryType == "edge" then
+                    tableName = "edges"
+                end
+                self.result[tableName][entry.index] = entryRecord.output
+            end
+            self.entryPositions[entry] = entryRecord
             x = x + xLength + xMargin
         end
-        middleY = middleY + 4 * yLayerLength
     end
-    for _,pos in pairs(layout.layers.reverse.vertex) do
-        local vertexEntry = layout.layers.entries[pos[1]][pos[2]]
-        LayerTreeLinkGenerator.run(self, vertexEntry)
+    local channelLayers = layout.channelLayers
+    local y = 0
+    for layerId=1,layout.layers.entries.count do
+        y = processChannelLayer(self, layerId, y)
+        local layer = self.layout.layers.entries[layerId]
+        local yMiddle = y + yLayerLength / 2
+        for rank=1,layer.count do
+            local entry = layer[rank]
+            local yHalfLength = typeToMinY[entry.type] / 2
+            local layerEntryPos = self.entryPositions[entry]
+            layerEntryPos.output.yMin = yMiddle - yHalfLength
+            layerEntryPos.output.yMax = yMiddle + yHalfLength
+        end
+        y = y + yLayerLength
     end
+    processChannelLayer(self, layout.layers.entries.count + 1, y)
+
+    for layerId=1,layout.layers.entries.count do
+        local layer = self.layout.layers.entries[layerId]
+        for rank=1,layer.count do
+            local entry = layer[rank]
+            local layerEntryPos = self.entryPositions[entry]
+            local yMin = layerEntryPos.output.yMin
+            for _,treeNode in pairs(layerEntryPos.inboundNodes) do
+                treeNode.y = yMin
+            end
+            local yMax = layerEntryPos.output.yMax
+            for _,treeNode in pairs(layerEntryPos.outboundNodes) do
+                treeNode.y = yMax
+            end
+        end
+    end
+
     return result
 end
 
