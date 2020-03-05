140192ba388065bb48c89ac814160dffe26f2565
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-03-05 21:14:21 +0100

    LayersInitialSorter: Remove StrictPoset usage for node order.

--- lua/layouts/layer/LayersInitialSorter.lua
+++ lua/layouts/layer/LayersInitialSorter.lua
@@ -20,7 +20,6 @@ local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
 local Iterator = require("lua/containers/utils/Iterator")
 local OrderedSet = require("lua/containers/OrderedSet")
 local ReversibleArray = require("lua/containers/ReversibleArray")
-local StrictPoset = require("lua/containers/StrictPoset")
 
 -- Helper class for sorting entries in their layers.
 --
@@ -41,7 +40,6 @@ local addPath
 local computeCouplingScore
 local createCouplings
 local getOrderByHighestCouplingCoefficients
-local initNode
 local makeRootIfNoPath
 local parseInput
 local sortLayers
@@ -82,16 +80,28 @@ local Node = ErrorOnInvalidRead.new{
 -- Layer sorting data.
 --
 -- Fields:
--- * nodes[nodeIndex]: Map of Node index -> Node object.
--- * processingOrder: StrictPoset of Nodes, holding the dependencies between nodes.
+-- * lowChannelNodes[nodeIndex]: Set of Node corresponding to a channel in the lower channel layer.
+-- * firstPass[nodeIndex]: Set of Node corresponding to an entry. Either roots, or directly computable from lowChannelNodes.
+-- * secondPass[nodeIndex]: Set of Node corresponding to an entry. Have dependencies to some entries in firstPass.
 --
 local LayerSortingData = ErrorOnInvalidRead.new{
+    -- Creates a new LayerSortingData object.
+    --
+    -- Returns: The new LayerSortingData object.
+    --
     new = function()
         return ErrorOnInvalidRead.new{
-            nodes = ErrorOnInvalidRead.new(),
-            processingOrder = StrictPoset.new(),
+            lowChannelNodes = ErrorOnInvalidRead.new(),
+            firstPass = ErrorOnInvalidRead.new(),
+            secondPass = ErrorOnInvalidRead.new(),
         }
     end,
+
+    -- Array of field names of LayerSortingData. Holds the right processing order for final coordinate generation.
+    NodeSetOrder = Array.new{"lowChannelNodes", "firstPass", "secondPass"},
+
+    -- Array of field names of LayerSortingData. These sets contains all the nodes associated to entries.
+    EntrySets = Array.new{"firstPass", "secondPass"},
 }
 
 -- Adds a path between two nodes.
@@ -151,12 +161,13 @@ parseInput = function(self)
     local prevNodes = nil
     for layerId=1,entries.count do
         local layerData = LayerSortingData.new()
-        local processingOrder = layerData.processingOrder
-        local nodes = layerData.nodes
+        local nodes = ErrorOnInvalidRead.new()
         self.layersSortingData[layerId] = layerData
         local layer = entries[layerId]
         for rank=1,layer.count do
-            initNode(layerData, layer[rank])
+            local node = Node.new(layer[rank])
+            layerData.firstPass[node.index] = node
+            nodes[node.index] = node
         end
 
         -- First pass: process channels with lower & higher entries.
@@ -167,7 +178,8 @@ parseInput = function(self)
             if lowEntries.count == 0 then
                 pureLowChannels[channelIndex] = highEntries
             else
-                local newNode = initNode(layerData, ErrorOnInvalidRead.new())
+                local newNode = Node.new(ErrorOnInvalidRead.new())
+                layerData.lowChannelNodes[newNode.index] = newNode
                 for i=1,lowEntries.count do
                     local entryNode = prevNodes[lowEntries[i]]
                     addPath(newNode, entryNode)
@@ -175,7 +187,6 @@ parseInput = function(self)
                 for i=1,highEntries.count do
                     local entryNode = nodes[highEntries[i]]
                     addPath(entryNode, newNode)
-                    processingOrder:addRelation(newNode, entryNode)
                 end
             end
         end
@@ -197,7 +208,8 @@ parseInput = function(self)
                     if entry ~= vertexEntry then
                         local entryNode = nodes[entry]
                         addPath(entryNode, vertexNode)
-                        processingOrder:addRelation(vertexNode, entryNode)
+                        layerData.firstPass[entryNode.index] = nil
+                        layerData.secondPass[entryNode.index] = entryNode
                     end
                 end
             end
@@ -231,20 +243,21 @@ createCouplings = function(self)
     end
 
     local entries = self.layersBuilder.layers.entries
+    local EntrySets = LayerSortingData.EntrySets
     for layerId=1,entries.count do
         local layer = entries[layerId]
-        local nodes = self.layersSortingData[layerId].nodes
-        for x=1,layer.count do
-            local entry = layer[x]
-            local node = nodes[entry]
-            local count = node.counts
-            local sqrCount = count * count
-            local it1 = Iterator.new(node.pathsToRoots)
-            local it2 = Iterator.new()
-            while it1:next() do
-                it2:copy(it1)
-                while it2:next() do
-                    result:addToCoupling(it1.key, it2.key, (it1.value * it2.value) / sqrCount)
+        for i=1,EntrySets.count do
+            local nodes = self.layersSortingData[layerId][EntrySets[i]]
+            for _,node in pairs(nodes) do
+                local count = node.counts
+                local sqrCount = count * count
+                local it1 = Iterator.new(node.pathsToRoots)
+                local it2 = Iterator.new()
+                while it1:next() do
+                    it2:copy(it1)
+                    while it2:next() do
+                        result:addToCoupling(it1.key, it2.key, (it1.value * it2.value) / sqrCount)
+                    end
                 end
             end
         end
@@ -280,19 +293,6 @@ getOrderByHighestCouplingCoefficients = function(roots, couplings)
     return result
 end
 
--- Initializes a new node in the sorter.
---
--- Args:
--- * layerSortingData: LayersInitialSorter object in which the new node will be inserted.
--- * nodeIndex: Index of the new node.
---
-initNode = function(layerSortingData, nodeIndex)
-    local newNode = Node.new(nodeIndex)
-    layerSortingData.nodes[nodeIndex] = newNode
-    layerSortingData.processingOrder:insert(newNode)
-    return newNode
-end
-
 -- Turns an entry into a root if it has no path to an existing root.
 --
 -- Args:
@@ -325,6 +325,7 @@ sortLayers = function(self)
         it = self.roots.entries[it]
     end
     local entries = self.layersBuilder.layers.entries
+    local NodeSetOrder = LayerSortingData.NodeSetOrder
     for layerId=1,entries.count do
         local layer = entries[layerId]
         local layerData = self.layersSortingData[layerId]
@@ -336,21 +337,21 @@ sortLayers = function(self)
                 positions[entry] = rank
             end
         end
-        local processingOrder = layerData.processingOrder:makeLinearExtension()
-        for pOrderRank=1,processingOrder.count do
-            local node = processingOrder[pOrderRank]
-            local nodeIndex = node.index
-            local rPos = rawget(rootPos, nodeIndex)
-            if rPos then
-                positions[nodeIndex] = rPos
-            else
-                local count = 0
-                local newPos = 0
-                for parentIndex in pairs(node.parents) do
-                    count = count + 1
-                    newPos = newPos + positions[parentIndex]
+        for i=1,NodeSetOrder.count do
+            local nodes = layerData[NodeSetOrder[i]]
+            for nodeIndex,node in pairs(nodes) do
+                local rPos = rawget(rootPos, nodeIndex)
+                if rPos then
+                    positions[nodeIndex] = rPos
+                else
+                    local count = 0
+                    local newPos = 0
+                    for parentIndex in pairs(node.parents) do
+                        count = count + 1
+                        newPos = newPos + positions[parentIndex]
+                    end
+                    positions[nodeIndex] = newPos / count
                 end
-                positions[nodeIndex] = newPos / count
             end
         end
         self.layersBuilder.layers:sortLayer(layerId, positions)
