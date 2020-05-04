3e165e4c45416a5e7fee3be22e02ded2ec0cd0da
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-05-04 23:02:15 +0200

    SimpleRenderer: coding style refactoring.

--- lua/SimpleRenderer.lua
+++ lua/SimpleRenderer.lua
@@ -17,44 +17,104 @@
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
 local LayoutParameters = require("lua/layouts/LayoutParameters")
 
+local Red = {r = 1, a = 1}
+local White = {r = 1, g = 1, b = 1, a = 1}
+local DarkGrey = {r = 0.1, g = 0.1, b = 0.1, a = 1}
+local LightGrey = {r = 0.2, g = 0.2, b = 0.2, a = 1}
+local DefaultLayoutParameters
+local Metatable
+local renderTree
+
 -- Class used to render graphs onto a LuaSurface.
 --
 -- RO properties:
 -- * surface: surface on which the graph is displayed.
 -- * rawPlayer: Lua player using this renderer.
 --
+-- Methods: see Metatable.__index.
+--
 local SimpleRenderer = ErrorOnInvalidRead.new{
-    new = nil,
+    -- Creates a new renderer.
+    --
+    -- Args:
+    -- * object to turn into a SimpleRenderer object.
+    --
+    -- Returns: the argument turned into a SimpleRenderer object.
+    --
+    new = function(object)
+        setmetatable(object, Metatable)
+        return object
+    end,
 }
 
--- Implementation stuff (private scope).
-local Impl = {
-    -- LayoutParameters object, used by all instances of SimpleRenderer.
-    LayoutParameters = LayoutParameters.new{
-        edgeMarginX = 0.2,
-        edgeMarginY = 0.2,
-        edgeMinX = 1.6,
-        edgeMinY = 1.6,
-        linkWidth = 0.25,
-        vertexMarginX = 0.2,
-        vertexMarginY = 0.2,
-        vertexMinX = 1.6,
-        vertexMinY = 1.6,
+-- LayoutParameters object, used by all instances of SimpleRenderer.
+DefaultLayoutParameters = LayoutParameters.new{
+    edgeMarginX = 0.2,
+    edgeMarginY = 0.2,
+    edgeMinX = 1.6,
+    edgeMinY = 1.6,
+    linkWidth = 0.25,
+    vertexMarginX = 0.2,
+    vertexMarginY = 0.2,
+    vertexMinX = 1.6,
+    vertexMinY = 1.6,
+}
+
+
+-- Metatable of the SimpleRenderer class.
+Metatable = {
+    __index = ErrorOnInvalidRead.new{
+        -- Draws a graph.
+        --
+        -- Args:
+        -- * self: SimpleRenderer instance.
+        -- * layout: LayerLayout object to draw.
+        --
+        draw = function(self, layout)
+            local layoutCoordinates = layout:computeCoordinates(DefaultLayoutParameters)
+            for vertexIndex,coords in pairs(layoutCoordinates.vertices) do
+                rendering.draw_rectangle({
+                    color = LightGrey,
+                    draw_on_ground = true,
+                    filled = true,
+                    left_top = {coords.xMin, coords.yMin},
+                    players = {self.rawPlayer},
+                    right_bottom = {coords.xMax, coords.yMax},
+                    surface = self.surface,
+                })
+                rendering.draw_sprite({
+                    players = {self.rawPlayer},
+                    sprite = vertexIndex.type .. "/" .. vertexIndex.rawPrototype.name,
+                    surface = self.surface,
+                    target = {(coords.xMin + coords.xMax) / 2, (coords.yMin + coords.yMax) / 2},
+                })
+            end
+            for edgeIndex,coords in pairs(layoutCoordinates.edges) do
+                rendering.draw_rectangle({
+                    color = DarkGrey,
+                    draw_on_ground = true,
+                    filled = true,
+                    left_top = {coords.xMin, coords.yMin},
+                    players = {self.rawPlayer},
+                    right_bottom = {coords.xMax, coords.yMax},
+                    surface = self.surface,
+                })
+                rendering.draw_sprite({
+                    players = {self.rawPlayer},
+                    sprite = edgeIndex.type .. "/" .. edgeIndex.rawPrototype.name,
+                    surface = self.surface,
+                    target = {(coords.xMin + coords.xMax) / 2, (coords.yMin + coords.yMax) / 2},
+                })
+            end
+            for rendererLink in pairs(layoutCoordinates.links) do
+                local color = White
+                if rendererLink.category == "backward" then
+                    color = Red
+                end
+                renderTree(self, rendererLink.tree, color)
+            end
+        end,
     },
-
-    -- Metatable of the SimpleRenderer class.
-    Metatable = {
-        __index = ErrorOnInvalidRead.new{
-            draw = nil, -- implemented later
-        },
-    },
-
-    renderTree = nil, -- implemented later
-
-    Red = {r = 1, a = 1},
-    White = {r = 1, g = 1, b = 1, a = 1},
-    DarkGrey = {r = 0.1, g = 0.1, b = 0.1, a = 1},
-    LightGrey = {r = 0.2, g = 0.2, b = 0.2, a = 1}
 }
 
 -- Renders a tree link.
@@ -66,7 +126,7 @@ local Impl = {
 -- * tree: The tree link to render.
 -- * color: Color used to draw the link.
 --
-function Impl.renderTree(self,tree,color)
+renderTree = function(self,tree,color)
     local from = {tree.x, tree.y}
     local count = 0
     for subtree in pairs(tree.children) do
@@ -80,7 +140,7 @@ function Impl.renderTree(self,tree,color)
             to = {subtree.x, subtree.y},
             width = 1,
         })
-        Impl.renderTree(self, subtree, color)
+        renderTree(self, subtree, color)
     end
     if rawget(tree, "parent") then
         count = count + 1
@@ -96,67 +156,6 @@ function Impl.renderTree(self,tree,color)
             target = from,
         })
     end
-end
-
--- Draws a graph.
---
--- Args:
--- * self: SimpleRenderer instance.
--- * layout: LayerLayout object to draw.
---
-function Impl.Metatable.__index.draw(self,layout)
-    local layoutCoordinates = layout:computeCoordinates(Impl.LayoutParameters)
-    for vertexIndex,coords in pairs(layoutCoordinates.vertices) do
-        rendering.draw_rectangle({
-            color = Impl.LightGrey,
-            draw_on_ground = true,
-            filled = true,
-            left_top = {coords.xMin, coords.yMin},
-            players = {self.rawPlayer},
-            right_bottom = {coords.xMax, coords.yMax},
-            surface = self.surface,
-        })
-        rendering.draw_sprite({
-            players = {self.rawPlayer},
-            sprite = vertexIndex.type .. "/" .. vertexIndex.rawPrototype.name,
-            surface = self.surface,
-            target = {(coords.xMin + coords.xMax) / 2, (coords.yMin + coords.yMax) / 2},
-        })
-    end
-    for edgeIndex,coords in pairs(layoutCoordinates.edges) do
-        rendering.draw_rectangle({
-            color = Impl.DarkGrey,
-            draw_on_ground = true,
-            filled = true,
-            left_top = {coords.xMin, coords.yMin},
-            players = {self.rawPlayer},
-            right_bottom = {coords.xMax, coords.yMax},
-            surface = self.surface,
-        })
-        rendering.draw_sprite({
-            players = {self.rawPlayer},
-            sprite = edgeIndex.type .. "/" .. edgeIndex.rawPrototype.name,
-            surface = self.surface,
-            target = {(coords.xMin + coords.xMax) / 2, (coords.yMin + coords.yMax) / 2},
-        })
-    end
-    for rendererLink in pairs(layoutCoordinates.links) do
-        local color = Impl.White
-        if rendererLink.category == "backward" then
-            color = Impl.Red
-        end
-        Impl.renderTree(self, rendererLink.tree, color)
-    end
-end
-
--- Creates a new renderer.
---
--- Args:
--- * object to turn into a SimpleRendererInstance.
---
-function SimpleRenderer.new(object)
-    setmetatable(object, Impl.Metatable)
-    return object
 end
 
 return SimpleRenderer
