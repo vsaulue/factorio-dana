702817c7a106b1c09c46fe93166ab4629fc29fe7
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-05-08 14:00:04 +0200

    CanvasRectangle: implement isCollidingWithAabb().

--- lua/canvas/objects/CanvasRectangle.lua
+++ lua/canvas/objects/CanvasRectangle.lua
@@ -14,8 +14,12 @@
 -- You should have received a copy of the GNU General Public License
 -- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
 
+local Aabb = require("lua/canvas/Aabb")
 local AbstractCanvasObject = require("lua/canvas/objects/AbstractCanvasObject")
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+
+local BaseMetatable = AbstractCanvasObject.Metatable
+local Metatable
 
 -- Class used to draw a rectangle on a Canvas.
 --
@@ -30,11 +34,27 @@ local CanvasRectangle = ErrorOnInvalidRead.new{
     -- Returns: The new CanvasRectangle object.
     --
     makeFromInitData = function(initData)
-        return AbstractCanvasObject.new{
+        return AbstractCanvasObject.new({
             id = rendering.draw_rectangle(initData),
             type = "rectangle",
-        }
+        }, Metatable)
     end,
 }
+
+-- Metatable of the CanvasRectangle class.
+Metatable = {
+    __index = {
+        -- Implements AbstractCanvasObject:isCollidingWithAabb()
+        --
+        isCollidingWithAabb = function(self, aabb)
+            local id = self.id
+            local ltPos = rendering.get_left_top(id).position
+            local rbPos = rendering.get_right_bottom(id).position
+            return ltPos.x <= aabb.xMax and ltPos.y <= aabb.yMax and rbPos.x >= aabb.xMin and rbPos.y >= aabb.yMin
+        end,
+	},
+}
+setmetatable(Metatable, { __index = BaseMetatable})
+setmetatable(Metatable.__index, { __index = BaseMetatable.__index})
 
 return CanvasRectangle
---
+++ lua/canvas/objects/tests/CanvasRectangle.lua
@@ -0,0 +1,119 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+_G.rendering = require("lua/canvas/objects/tests/MockRendering")
+
+local Aabb = require("lua/canvas/Aabb")
+local CanvasRectangle = require("lua/canvas/objects/CanvasRectangle")
+
+describe("CanvasRectangle", function()
+    local object
+
+    before_each(function()
+        object = CanvasRectangle.makeFromInitData{
+            left_top = {
+                x = -1,
+                y = -2,
+            },
+            right_bottom = {
+                x = 3,
+                y = 4,
+            },
+        }
+    end)
+
+    after_each(function()
+        object = nil
+    end)
+
+    it(":close()", function()
+        local id = object.id
+        object:close()
+        assert.are.equals(id.type, "destroyed")
+    end)
+
+    describe(":isCollidingWithAabb()", function()
+        it("-- strictly over", function()
+            local aabb = Aabb.new{
+                xMin = 0,
+                xMax = 1,
+                yMin = -2.5,
+                yMax = -2.1,
+            }
+            assert.is_false(object:isCollidingWithAabb(aabb))
+        end)
+
+        it("-- strictly under", function()
+            local aabb = Aabb.new{
+                xMin = 0,
+                xMax = 1,
+                yMin = 4.1,
+                yMax = 5,
+            }
+            assert.is_false(object:isCollidingWithAabb(aabb))
+        end)
+
+        it("-- strictly left", function()
+            local aabb = Aabb.new{
+                xMin = -3,
+                xMax = -1.01,
+                yMin = 0,
+                yMax = 1,
+            }
+            assert.is_false(object:isCollidingWithAabb(aabb))
+        end)
+
+        it("-- strictly right", function()
+            local aabb = Aabb.new{
+                xMin = 3.01,
+                xMax = 5,
+                yMin = 0,
+                yMax = 1,
+            }
+            assert.is_false(object:isCollidingWithAabb(aabb))
+        end)
+
+        it("-- strictly included", function()
+            local aabb = Aabb.new{
+                xMin = -0.99,
+                xMax = 2.99,
+                yMin = -1.99,
+                yMax = 3.99,
+            }
+            assert.is_true(object:isCollidingWithAabb(aabb))
+        end)
+
+        it("-- strictly including", function()
+            local aabb = Aabb.new{
+                xMin = -1.1,
+                xMax = 3.1,
+                yMin = -2.1,
+                yMax = 4.1,
+            }
+            assert.is_true(object:isCollidingWithAabb(aabb))
+        end)
+
+        it("-- overlap", function()
+            local aabb = Aabb.new{
+                xMin = -10,
+                xMax = -0.99,
+                yMin = -1,
+                yMax = 5,
+            }
+            assert.is_true(object:isCollidingWithAabb(aabb))
+        end)
+    end)
+end)
