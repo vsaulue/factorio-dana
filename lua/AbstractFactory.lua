ec5aff06b6937a8888f7f1ab6f6d47d183d67a25
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-06-17 15:03:45 +0200

    AbstractFactory: add make() method.

--- lua/AbstractFactory.lua
+++ lua/AbstractFactory.lua
@@ -20,11 +20,18 @@ local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
 local cLogger = ClassLogger.new{className = "AbstractFactory"}
 
 local Metatable
+local getClassOfObject
+local assertClassField
 
--- Utility class to restore metatables of virtual/abstract types.
+-- Utility class to create virtual objects and/or restore their metatables.
+--
+-- Classes usable by this factory are tables with the following fields:
+-- * setmetatable: restore the metatable of the object, and all its owned objects.
+-- * new: creates a new object (optional: required only if enableMake is set).
 --
 -- RO Fields:
 -- * classes[name]: Map of class tables, indexed by their names.
+-- * enableMake: boolean to enable the make() method.
 -- * getClassNameOfObject (abstract): Function (NOT method) to get the class name of an object.
 --       Note that at this point, the object doesn't have a metatable (this function can't be implemented with
 --       any __index field of the argument).
@@ -42,6 +49,7 @@ local AbstractFactory = ErrorOnInvalidRead.new{
     new = function(object)
         cLogger:assertField(object, "getClassNameOfObject")
         object.classes = ErrorOnInvalidRead.new()
+        object.enableMake = object.enableMake or false
         setmetatable(object, Metatable)
         return object
     end,
@@ -50,6 +58,21 @@ local AbstractFactory = ErrorOnInvalidRead.new{
 -- Metatable of the AbstractFactory class.
 Metatable = {
     __index = ErrorOnInvalidRead.new{
+        -- Creates a new instance of one of the wrapped type.
+        --
+        -- The exact type & constructor is determined with getClassNameOfObject.
+        --
+        -- Args:
+        -- * self: AbstractFactory object.
+        -- * object: Constructor argument.
+        --
+        -- Returns: The new object constructed from the argument.
+        --
+        make = function(self, object)
+            cLogger:assert(self.enableMake, "make() can't be used with 'enableMake == false'")
+            return getClassOfObject(self, object).new(object)
+        end,
+
         -- Registers a new class for this factory.
         --
         -- Args:
@@ -59,7 +82,10 @@ Metatable = {
         --
         registerClass = function(self, className, classTable)
             cLogger:assert(not rawget(self.classes, className), "Duplicate class name: " .. className)
-            cLogger:assert(classTable.setmetatable, "Invalid class table (missing 'setmetatable' function).")
+            if self.enableMake then
+                assertClassField(classTable, "new")
+            end
+            assertClassField(classTable, "setmetatable")
             self.classes[className] = classTable
         end,
 
@@ -70,11 +96,37 @@ Metatable = {
         -- * object: table to modify.
         --
         restoreMetatable = function(self, object)
-            local className = self.getClassNameOfObject(object)
-            cLogger:assert(className, "Unable to retrieve the class name of the object.")
-            self.classes[className].setmetatable(object)
+            getClassOfObject(self, object).setmetatable(object)
         end,
     },
 }
+
+-- Asserts that a field is defined in the given class table.
+--
+-- Args:
+-- * classTable: Class table to check.
+-- * fieldName: Key of the field to check.
+--
+assertClassField = function(classTable, fieldName)
+    if not classTable[fieldName] then
+        cLogger:error("Invalid class table (missing '" .. fieldName .. "' function).")
+    end
+end
+
+-- Gets the class table of a given object.
+--
+-- The object does not necessarily have a metatable set at this point.
+--
+-- Args:
+-- * self: AbstractFactory object.
+-- * object: An object of any type managed by this factory.
+--
+-- Returns: The class table of the object.
+--
+getClassOfObject = function(self, object)
+    local className = self.getClassNameOfObject(object)
+    cLogger:assert(className, "Unable to retrieve the class name of the object.")
+    return self.classes[className]
+end
 
 return AbstractFactory
