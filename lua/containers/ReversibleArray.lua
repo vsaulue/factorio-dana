f8bf46c9f2e4c0cf49a0566736d19b68c4c5dfba
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-07-01 18:09:23 +0200

    ReversibleArray: coding style refactoring.

--- lua/containers/ReversibleArray.lua
+++ lua/containers/ReversibleArray.lua
@@ -16,6 +16,9 @@
 
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
 
+local Metatable
+local iteratorNext
+
 -- Container implementing a reversible array.
 --
 -- A ReversibleArray is an ordered set, which can easily be accessed both way:
@@ -29,152 +32,139 @@ local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
 -- * (int): the value of the N-th
 -- * (other): the index of this value.
 --
--- Methods:
--- * getLowHighValues: Returns the two input values, reordered to match their indices' order.
--- * popBack: Removes and returns the last value of this ReversibleArray.
--- * pushBack: Adds a new value at the end of this ReversibleArray.
--- * pushBackIfNotPresent: Append a value if it's not already present, else does nothing.
--- * sort: sorts the array in place.
---
 local ReversibleArray = ErrorOnInvalidRead.new{
-    new = nil, -- implemented later
-}
-
--- Implementation stuff (private scope).
-local Impl = ErrorOnInvalidRead.new{
-    -- Function used to iterate through a ReversibleArray object.
+    -- Creates a new empty ReversibleArray.
     --
-    -- Args:
-    -- * self: ReversibleArray object.
-    -- * index: Index of the element accessed in the previous iteration step.
+    -- Returns: The new ReversibleArray object.
     --
-    -- Returns:
-    -- * The index of the next element (or nil if the end of the array was reached).
-    -- * The value of the next element (or nil if the end of the array was reached).
-    --
-    iteratorNext = function(self, index)
-        local nextIndex = index + 1
-        local nextValue = nil
-        if nextIndex <= self.count then
-            nextValue = self[nextIndex]
-        else
-            nextIndex = nil
-        end
-        return nextIndex, nextValue
+    new = function()
+        local result = {
+            count = 0,
+        }
+        setmetatable(result, Metatable)
+        return result
     end,
-
-    -- Metatable of the LayerLink class.
-    Metatable = {
-        __index = ErrorOnInvalidRead.new{
-            -- Returns the two input values, reordered to match their indices' order.
-            --
-            -- Args:
-            -- * self: Couplings object.
-            -- * valueA: Some value.
-            -- * valueB: Another value.
-            --
-            -- Returns:
-            -- * The value with the lower index.
-            -- * The value with the greater index.
-            --
-            getLowHighValues = function(self, valueA, valueB)
-                local idA = self[valueA]
-                local idB = self[valueB]
-                local lowValue = valueB
-                local highValue = valueA
-                if idA < idB then
-                    lowValue = valueA
-                    highValue = valueB
-                end
-                return lowValue, highValue
-            end,
-
-            -- Removes and returns the last value of this ReversibleArray.
-            --
-            -- Args:
-            -- * self: ReversibleArray object.
-            --
-            -- Returns: The value removed at the end of this array.
-            --
-            popBack = function(self)
-                local count = self.count
-                assert(count > 0, "ReversibleArray: attempt to popBack() an empty array.")
-                local result = self[count]
-                self[count] = nil
-                self[result] = nil
-                self.count = count - 1
-                return result
-            end,
-
-            -- Adds a new value at the end of this ReversibleArray.
-            --
-            -- Args:
-            -- * self: ReversibleArrayobject.
-            -- * value: The new value to add.
-            --
-            pushBack = function(self, value)
-                local valueType = type(x)
-                assert(value, "ReversibleArray: nil value is not supported.")
-                assert(valueType ~= "number", "ReversibleArray: number values are not supported.")
-                assert(valueType ~= "string", "ReversibleArray: string values are not supported.")
-                assert(not rawget(self, value), "ReversibleArray: duplicate values.")
-
-                local count = self.count + 1
-                self.count = count
-                self[count] = value
-                self[value] = count
-            end,
-
-            -- Append a value if it's not already present, else does nothing.
-            --
-            -- Args:
-            -- * self: ReversibleArrayobject.
-            -- * value: The value to append, if it's not already present.
-            --
-            pushBackIfNotPresent = function(self, value)
-                if not rawget(self, value) then
-                    self:pushBack(value)
-                end
-            end,
-
-            -- Sorts this reversible array in place.
-            --
-            -- Args:
-            -- * self: ReversibleArray object to sort.
-            -- * weights[arrayValue]: a map giving a score for each value in the array. Values will be sorted
-            -- from lowest to greatest scores.
-            --
-            sort = function(self, weights)
-                local compare = function(a,b)
-                    local w = weights
-                    return w[a] < w[b]
-                end
-                table.sort(self, compare)
-                for i=1,self.count do
-                    local value = self[i]
-                    self[value] = i
-                end
-            end,
-        },
-    },
 }
 
-function Impl.Metatable.__ipairs(self)
-    return Impl.iteratorNext, self, 0
-end
+-- Metatable of the ReversibleArray class.
+Metatable = {
+    __index = ErrorOnInvalidRead.new{
+        -- Returns the two input values, reordered to match their indices' order.
+        --
+        -- Args:
+        -- * self: ReversibleArray object.
+        -- * valueA: Some value.
+        -- * valueB: Another value.
+        --
+        -- Returns:
+        -- * The value with the lower index.
+        -- * The value with the greater index.
+        --
+        getLowHighValues = function(self, valueA, valueB)
+            local idA = self[valueA]
+            local idB = self[valueB]
+            local lowValue = valueB
+            local highValue = valueA
+            if idA < idB then
+                lowValue = valueA
+                highValue = valueB
+            end
+            return lowValue, highValue
+        end,
 
-Impl.Metatable.__pairs = Impl.Metatable.__ipairs
+        -- Removes and returns the last value of this ReversibleArray.
+        --
+        -- Args:
+        -- * self: ReversibleArray object.
+        --
+        -- Returns: The value removed at the end of this array.
+        --
+        popBack = function(self)
+            local count = self.count
+            assert(count > 0, "ReversibleArray: attempt to popBack() an empty array.")
+            local result = self[count]
+            self[count] = nil
+            self[result] = nil
+            self.count = count - 1
+            return result
+        end,
 
--- Creates a new empty ReversibleArray.
+        -- Adds a new value at the end of this ReversibleArray.
+        --
+        -- Args:
+        -- * self: ReversibleArray object.
+        -- * value: The new value to add.
+        --
+        pushBack = function(self, value)
+            local valueType = type(x)
+            assert(value, "ReversibleArray: nil value is not supported.")
+            assert(valueType ~= "number", "ReversibleArray: number values are not supported.")
+            assert(valueType ~= "string", "ReversibleArray: string values are not supported.")
+            assert(not rawget(self, value), "ReversibleArray: duplicate values.")
+
+            local count = self.count + 1
+            self.count = count
+            self[count] = value
+            self[value] = count
+        end,
+
+        -- Append a value if it's not already present, else does nothing.
+        --
+        -- Args:
+        -- * self: ReversibleArrayobject.
+        -- * value: The value to append, if it's not already present.
+        --
+        pushBackIfNotPresent = function(self, value)
+            if not rawget(self, value) then
+                self:pushBack(value)
+            end
+        end,
+
+        -- Sorts this reversible array in place.
+        --
+        -- Args:
+        -- * self: ReversibleArray object to sort.
+        -- * weights[arrayValue]: a map giving a score for each value in the array. Values will be sorted
+        -- from lowest to greatest scores.
+        --
+        sort = function(self, weights)
+            local compare = function(a,b)
+                local w = weights
+                return w[a] < w[b]
+            end
+            table.sort(self, compare)
+            for i=1,self.count do
+                local value = self[i]
+                self[value] = i
+            end
+        end,
+    },
+
+    __ipairs = function(self)
+        return iteratorNext, self, 0
+    end,
+}
+Metatable.__pairs = Metatable.__ipairs
+
+-- Function used to iterate through a ReversibleArray object.
 --
--- Returns: The new ReversibleArray object.
+-- Args:
+-- * self: ReversibleArray object.
+-- * index: Index of the element accessed in the previous iteration step.
 --
-function ReversibleArray.new()
-    local result = {
-        count = 0,
-    }
-    setmetatable(result, Impl.Metatable)
-    return result
+-- Returns:
+-- * The index of the next element (or nil if the end of the array was reached).
+-- * The value of the next element (or nil if the end of the array was reached).
+--
+iteratorNext = function(self, index)
+    local nextIndex = index + 1
+    local nextValue = nil
+    if nextIndex <= self.count then
+        nextValue = self[nextIndex]
+    else
+        nextIndex = nil
+    end
+    return nextIndex, nextValue
 end
 
 return ReversibleArray
