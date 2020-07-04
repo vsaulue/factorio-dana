6ef5e713c32804dea79c45ddbd946ac2682f0b03
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-07-04 11:07:38 +0200

    ProductFilterEditor: add maxDepth editor.

--- lua/apps/query/gui/ProductFilterEditor.lua
+++ lua/apps/query/gui/ProductFilterEditor.lua
@@ -18,9 +18,14 @@ local AbstractFilterEditor = require("lua/apps/query/gui/AbstractFilterEditor")
 local CheckboxUpdater = require("lua/gui/CheckboxUpdater")
 local ClassLogger = require("lua/logger/ClassLogger")
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+local GuiAlign = require("lua/gui/GuiAlign")
+local GuiElement = require("lua/gui/GuiElement")
 local IntermediateSetEditor = require("lua/apps/query/gui/IntermediateSetEditor")
 
 local cLogger = ClassLogger.new{className = "ProductFilterEditor"}
+
+local DepthCheckbox
+local DepthField
 
 -- Filter editor for the ProductQueryFilter class.
 --
@@ -28,6 +33,8 @@ local cLogger = ClassLogger.new{className = "ProductFilterEditor"}
 --
 -- RO Fields:
 -- * allowOtherCheckbox: CheckboxUpdater handling the allowOtherIngredients field.
+-- * depthCheckbox: DepthCheckbox object enabling/disabling the maxDepth parameter.
+-- * depthField: DepthField object setting the maxDepth value.
 -- * setEditor: IntermediateSetEditor object used on the source set.
 --
 local ProductFilterEditor = ErrorOnInvalidRead.new{
@@ -44,6 +51,7 @@ local ProductFilterEditor = ErrorOnInvalidRead.new{
         if object.filter.filterType ~= "product" then
             cLogger:error("Invalid filter type: " .. object.filter.filterType)
         end
+        -- Set selector.
         object.root.add{
             type = "label",
             caption = {"dana.apps.query.productFilterEditor.sourceSetTitle"},
@@ -54,6 +62,7 @@ local ProductFilterEditor = ErrorOnInvalidRead.new{
             output = object.filter.sourceIntermediates,
             parent = object.root,
         }
+        ----------------
         object.root.add{
             type = "line",
             direction = "horizontal",
@@ -63,6 +72,7 @@ local ProductFilterEditor = ErrorOnInvalidRead.new{
             caption = {"dana.apps.query.productFilterEditor.otherOptions"},
             style = "frame_title",
         }
+        -- Allow other ingredients.
         object.allowOtherCheckbox = CheckboxUpdater.new{
             object = object.filter,
             field = "allowOtherIngredients",
@@ -70,6 +80,30 @@ local ProductFilterEditor = ErrorOnInvalidRead.new{
                 type = "checkbox",
                 caption = {"dana.apps.query.productFilterEditor.allowOtherIngredients"},
                 state = object.filter.allowOtherIngredients,
+            },
+        }
+        -- Maximum depth
+        local depthFlow = object.root.add{
+            type = "flow",
+            direction = "horizontal",
+        }
+        object.depthCheckbox = DepthCheckbox.new{
+            filterEditor = object,
+            rawElement = GuiAlign.makeVerticallyCentered(depthFlow, {
+                type = "checkbox",
+                caption = {"dana.apps.query.productFilterEditor.maxDepth"},
+                state = false,
+            }),
+        }
+        object.depthField = DepthField.new{
+            filterEditor = object,
+            rawElement = depthFlow.add{
+                type = "textfield",
+                allow_negative = false,
+                numeric = true,
+                style = "short_number_textfield",
+                text = "1",
+                enabled = false,
             },
         }
         return object
@@ -84,7 +118,48 @@ local ProductFilterEditor = ErrorOnInvalidRead.new{
         ErrorOnInvalidRead.setmetatable(object)
         IntermediateSetEditor.setmetatable(object.setEditor)
         CheckboxUpdater.setmetatable(object.allowOtherCheckbox)
+        DepthCheckbox.setmetatable(object.depthCheckbox)
+        DepthField.setmetatable(object.depthField)
     end,
+}
+
+-- Checkbox to enable the maxDepth parameter.
+--
+-- RO Fields:
+-- * filterEditor: ProductFilterEditor object owning this checkbox.
+--
+DepthCheckbox = GuiElement.newSubclass{
+    className = "ProductFilterEditor/DepthCheckbox",
+    mandatoryFields = {"filterEditor"},
+    __index = {
+        onCheckedStateChanged = function(self, event)
+            local state = event.element.state
+            local depthField = self.filterEditor.depthField
+            local filter = self.filterEditor.filter
+
+            depthField.rawElement.enabled = state
+            if state then
+                filter.maxDepth = tonumber(depthField.rawElement.text)
+            else
+                filter.maxDepth = nil
+            end
+        end,
+    },
+}
+
+-- Textfield to set the maxDepth value.
+--
+-- RO Fields:
+-- * filterEditor; ProductFilterEditor object owning this textfield.
+--
+DepthField = GuiElement.newSubclass{
+    className = "ProductFilterEditor/DepthField",
+    mandatoryFields = {"filterEditor"},
+    __index = {
+        onTextChanged = function(self, event)
+            self.filterEditor.filter.maxDepth = tonumber(event.element.text)
+        end,
+    },
 }
 
 AbstractFilterEditor.Factory:registerClass("product", ProductFilterEditor)
