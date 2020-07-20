8c312fc19ad02a734c18d950cdab543579670000
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-07-20 20:17:57 +0200

    LayerXPass: major overhaul of the algorithm.
    Usage of 1-way barycenter from the largest layer.

--- lua/layouts/layer/coordinates/LayerXPass.lua
+++ lua/layouts/layer/coordinates/LayerXPass.lua
@@ -14,7 +14,15 @@
 -- You should have received a copy of the GNU General Public License
 -- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
 
+local Array = require("lua/containers/Array")
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+local LayerXPassBlock = require("lua/layouts/layer/coordinates/LayerXPassBlock")
+local OrderedSet = require("lua/containers/OrderedSet")
+
+local barycenterPass
+local firstPass
+local fixConflicts
+local Parsers
 
 -- Helper library to assign an X coordinate to entries in LayerCoordinateGenerator.
 --
@@ -25,49 +33,202 @@ local LayerXPass = ErrorOnInvalidRead.new{
     -- * self: LayerCoordinateGenerator object.
     --
     run = function(layerCoordinateGenerator)
-        local params = layerCoordinateGenerator.params
-        local typeToMinX = ErrorOnInvalidRead.new{
-            edge = params.edgeMinX,
-            linkNode = params.linkWidth,
-            vertex = params.vertexMinX,
-        }
-        local typeToMarginX = ErrorOnInvalidRead.new{
-            edge = params.edgeMarginX,
-            linkNode = 0,
-            vertex = params.vertexMarginX,
-        }
-        local entries = layerCoordinateGenerator.layout.layers.entries
-        local xLengthMax = - math.huge
-        local layerXLengths = ErrorOnInvalidRead.new()
-        for layerId=1,entries.count do
-            local layer = entries[layerId]
-            local x = 0
-            for rank=1,layer.count do
-                local entry = layer[rank]
-                local entryType = entry.type
-                local maxSlotsCount = math.max(entry.lowSlots.count, entry.highSlots.count)
-                local xLength = math.max(typeToMinX[entryType], params.linkWidth * maxSlotsCount)
-                local xMargin = typeToMarginX[entryType]
-                x = x + xMargin
-                local entryRecord = layerCoordinateGenerator.entryPositions[entry]
-                entryRecord:initX(x, xLength, xMargin)
-                x = x + xLength + xMargin
-            end
-            if x > xLengthMax then
-                xLengthMax = x
-            end
-            layerXLengths[layerId] = x
+        local layerIdMaxLength = firstPass(layerCoordinateGenerator)
+
+        local layout = layerCoordinateGenerator.layout
+        local entryPositions = layerCoordinateGenerator.entryPositions
+
+        for layerId=layerIdMaxLength-1, 1, -1 do
+            barycenterPass(layerCoordinateGenerator, layerId, Parsers.HighToLow)
         end
 
-        -- Center
-        for layerId=1,entries.count do
-            local layer = entries[layerId]
-            local xDelta = (xLengthMax - layerXLengths[layerId]) / 2
-            for rank=1,layer.count do
-                layerCoordinateGenerator.entryPositions[layer[rank]]:translateX(xDelta)
-            end
+        for layerId=layerIdMaxLength+1, layout.layers.entries.count do
+            barycenterPass(layerCoordinateGenerator, layerId, Parsers.LowToHigh)
         end
     end,
+}
+
+-- Runs a barycenter algorithm to computes the X coordinates in a layer.
+--
+-- This algorithm initially computes an X offset for each entry as the barycenter of connected entries
+-- from an adjacent layer (the `parser` argument determines the one to use). Then a correction pass is
+-- run to fix any overlap/permutation.
+--
+-- Args:
+-- * layerCoordinateGenerator: LayerCoordinateGenerator object.
+-- * layerId: Index of the layer to modify.
+-- * parser: An object from the `Parsers` table, used to select which adjacent layer to use for input coordinates.
+--
+barycenterPass = function(layerCoordinateGenerator, layerId, parser)
+    local layout = layerCoordinateGenerator.layout
+    local layer = layout.layers.entries[layerId]
+    if layer.count > 0 then
+        local entryPositions = layerCoordinateGenerator.entryPositions
+        local channelLayer = layout.channelLayers[layerId + parser.ChannelLayerOffset]
+
+        local channelsAvgX = {}
+        for channelIndex,entries in pairs(channelLayer[parser.ChannelLayerFar]) do
+            local totalX = 0
+            local count = entries.count
+            if count > 0 then
+                for i=1,count do
+                    local entry = entries[i]
+                    totalX = totalX + entryPositions[entry]:getNode(channelIndex, parser.EntryPosIsFarLow).x
+                end
+                channelsAvgX[channelIndex] = totalX / count
+            end
+        end
+
+        local blocks = OrderedSet.new()
+        local previousBlock = nil
+        local noParents = Array.new()
+        for rank=1,layer.count do
+            local entry = layer[rank]
+            local entryPos = entryPositions[entry]
+            local count = 0
+            local oldXMin = entryPos.output.xMin
+            local newXMin = 0
+            for channelIndex,node in pairs(entryPos[parser.EntryPosNear]) do
+                local channelX = channelsAvgX[channelIndex]
+                if channelX then
+                    local offsetX = node.x - oldXMin
+                    newXMin = newXMin + channelX - offsetX
+                    count = count + 1
+                end
+            end
+
+            if count > 0 then
+                local newBlock = LayerXPassBlock.make(entryPos, newXMin / count, count)
+                if noParents.count > 0 then
+                    if previousBlock then
+                        -- Note: maybe spacing them evenly between newBlock & previousBlock might be nicer.
+                        previousBlock:appendEntries(noParents)
+                    else
+                        noParents = newBlock:prependEntries(noParents)
+                    end
+                    noParents.count = 0
+                end
+                blocks:pushBack(newBlock)
+                previousBlock = newBlock
+            else
+                noParents:pushBack(entryPos)
+            end
+        end
+
+
+        if previousBlock then
+            previousBlock:appendEntries(noParents)
+
+            fixConflicts(blocks)
+
+            local End = blocks.End
+            local block = blocks.forward[blocks.Begin]
+            while block ~= End do
+                block:apply()
+                block = blocks.forward[block]
+            end
+        end
+    end
+end
+
+-- Initializes the X coordinates of each entries, witha  simple "left align" algorithm.
+--
+-- Args:
+-- * LayerCoordinateGenerator: LayerCoordinateGenerator object.
+--
+-- Returns: The index of the widest layer (X length).
+--
+firstPass = function(layerCoordinateGenerator)
+    local params = layerCoordinateGenerator.params
+    local typeToMinX = ErrorOnInvalidRead.new{
+        edge = params.edgeMinX,
+        linkNode = params.linkWidth,
+        vertex = params.vertexMinX,
+    }
+    local typeToMarginX = ErrorOnInvalidRead.new{
+        edge = params.edgeMarginX,
+        linkNode = 0,
+        vertex = params.vertexMarginX,
+    }
+    local entries = layerCoordinateGenerator.layout.layers.entries
+    local xLengthMax = - math.huge
+    local layerIdMaxLength = nil
+    for layerId=1,entries.count do
+        local layer = entries[layerId]
+        local x = 0
+        for rank=1,layer.count do
+            local entry = layer[rank]
+            local entryType = entry.type
+            local maxSlotsCount = math.max(entry.lowSlots.count, entry.highSlots.count)
+            local xLength = math.max(typeToMinX[entryType], params.linkWidth * maxSlotsCount)
+            local xMargin = typeToMarginX[entryType]
+            x = x + xMargin
+            local entryRecord = layerCoordinateGenerator.entryPositions[entry]
+            entryRecord:initX(x, xLength, xMargin)
+            x = x + xLength + xMargin
+        end
+        if x > xLengthMax then
+            xLengthMax = x
+            layerIdMaxLength = layerId
+        end
+    end
+    return layerIdMaxLength
+end
+
+-- Runs a conflict resolution algorithm on a sequence of blocks.
+--
+-- This algorithm merges/moves blocks around to ensure that X coordinates of blocks follow the order in the set
+-- and don't overlap.
+--
+-- Args:
+-- * blocks: OrderedSet of LayerXPassBlock to fix.
+--
+fixConflicts = function(blocks)
+    local End = blocks.End
+    local forward = blocks.forward
+    repeat
+        local noConflicts = true
+        local currentBlock = forward[blocks.Begin]
+        local nextBlock = forward[currentBlock]
+        while nextBlock ~= End do
+            local cMax = currentBlock.xCenterOfMass + currentBlock.xMaxOffset
+            local nMin = nextBlock.xCenterOfMass + nextBlock.xMinOffset
+            if cMax > nMin then
+                noConflicts = false
+                currentBlock:mergeWith(nextBlock)
+                blocks:remove(nextBlock)
+            else
+                currentBlock = nextBlock
+            end
+            nextBlock = forward[currentBlock]
+        end
+    until noConflicts
+end
+
+-- Records used determine which adjacent layer to use in the barycenter pass.
+--
+-- Fields:
+-- * ChannelLayerFar: Field name in ChannelLayer. Entries to read in the input layer.
+-- * ChannelLayerOffset: Value to add to the edited layer index to get the relevant channel layer.
+-- * EntryPosNear: Field name in LayerEntryPosition. Nodes to read in the edited layer.
+-- * EntryPosIsFarLow: True to read the low nodes in the input layer, false otherwise.
+--
+Parsers = ErrorOnInvalidRead.new{
+    -- Object to compute the coordinates from the adjacent layer with higher index.
+    HighToLow = ErrorOnInvalidRead.new{
+        ChannelLayerFar = "highEntries",
+        ChannelLayerOffset = 1,
+        EntryPosNear = "highNodes",
+        EntryPosIsFarLow = true,
+    },
+
+    -- Object to compute the coordinates from the adjacent layer with lower index.
+    LowToHigh = ErrorOnInvalidRead.new{
+        ChannelLayerFar = "lowEntries",
+        ChannelLayerOffset = 0,
+        EntryPosNear = "lowNodes",
+        EntryPosIsFarLow = false,
+    },
 }
 
 return LayerXPass
---
+++ lua/layouts/layer/coordinates/LayerXPassBlock.lua
@@ -0,0 +1,167 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local Array = require("lua/containers/Array")
+local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+
+local mergeArrays
+local Metatable
+
+-- Class holding contiguous LayerEntryPosition objects within a layer.
+--
+-- RO Fields:
+-- * entries: Array of entries in this block.
+-- * xCenterOfMass: Absolute X coordinate of the center of mass of this block.
+-- * xMinOffset: Offset to add to xCenterOfMass to get the lower bound on the X axis.
+-- * xMaxOffset: Offset to add to xCenterOfMass to get the upper bound on the X axis.
+-- * weight: Weight of this block.
+--
+local LayerXPassBlock = ErrorOnInvalidRead.new{
+    -- Creates a new block, wraping a single LayerEntryPosition object.
+    --
+    -- Args:
+    -- * entryPos: LayerEntryPosition object to wrap.
+    -- * xMin: Lower bound of the X coordinates of this block.
+    -- * weight: Weight of the new block.
+    --
+    -- Returns: The new block wrapping the argument entry.
+    --
+    make = function(entryPos, xMin, weight)
+        local xHalfLength = entryPos:getXLength() / 2
+        local xCenter = xMin + xHalfLength
+        local xOffset = xHalfLength + entryPos.xMargin
+        local result = {
+            entries = Array.new(),
+            xCenterOfMass = xCenter,
+            xMinOffset = - xOffset,
+            xMaxOffset = xOffset,
+            weight = weight,
+        }
+        result.entries:pushBack(entryPos)
+        setmetatable(result, Metatable)
+        return result
+    end,
+}
+
+-- Metatable of the LayerXPassBlock class.
+Metatable = {
+    __index = ErrorOnInvalidRead.new{
+        -- Appends an array of entries to this block.
+        --
+        -- The entries already present in the block won't be moved.
+        --
+        -- Args:
+        -- * self: LayerXPassBlock object.
+        -- * entryArray: Array of LayerEntryPosition to append to this block.
+        --
+        appendEntries = function(self, entryArray)
+            local xMaxOffsetDelta = 0
+            local entries = self.entries
+            local baseCount = entries.count
+            for i=1,entryArray.count do
+                local newEntry = entryArray[i]
+                entries[baseCount+i] = newEntry
+                xMaxOffsetDelta = xMaxOffsetDelta + newEntry:getXLength() + 2 * newEntry.xMargin
+            end
+            entries.count = baseCount + entryArray.count
+            self.xMaxOffset = self.xMaxOffset + xMaxOffsetDelta
+        end,
+
+        -- Uses the coordinates of this block to overwrite the X coordinates of its entries.
+        --
+        -- Args:
+        -- * self: LayerXPassBlock object.
+        --
+        apply = function(self)
+            local entries = self.entries
+            local x = self.xMinOffset + self.xCenterOfMass
+            for i=1,entries.count do
+                local entryPos = entries[i]
+                local xMargin = entryPos.xMargin
+                local xLength = entryPos:getXLength()
+                x = x + xMargin
+                entryPos:translateX(x - entryPos.output.xMin)
+                x = x + xLength + xMargin
+            end
+        end,
+
+        -- Append the given block to the current block.
+        --
+        -- Args:
+        -- * self: LayerXPassBlock object.
+        -- * nextBlock: Other LayerXPassBlock object to append to `self`.
+        --
+        mergeWith = function(self, nextBlock)
+            local entries = self.entries
+            mergeArrays(entries, nextBlock.entries)
+
+            local weight = self.weight
+            local nWeight = nextBlock.weight
+            local totalWeight = weight + nWeight
+            local center = self.xCenterOfMass
+            local nCenter = nextBlock.xCenterOfMass
+            local centerDist = self.xMaxOffset - nextBlock.xMinOffset
+            self.xCenterOfMass = (weight * center + nWeight * nCenter) / totalWeight
+            self.xMinOffset = self.xMinOffset - nWeight / totalWeight * centerDist
+            self.xMaxOffset = nextBlock.xMaxOffset + weight / totalWeight * centerDist
+            self.weight = totalWeight
+        end,
+
+        -- Prepents an array of entries to this block.
+        --
+        -- The entries already present in the block won't be moved.
+        --
+        -- The array passed in argument will be used by the block, so it can't be used anymore by the caller.
+        --
+        -- Args:
+        -- * self: LayerXPassBlock object.
+        -- * entryArray: Array containing the entries to prepend. It will be used by the block.
+        --
+        -- Returns: The old array of entries.
+        --
+        prependEntries = function(self, entryArray)
+            local oldEntries = self.entries
+            local xMinOffsetDelta = 0
+            for i=1,entryArray.count do
+                local entry = entryArray[i]
+                xMinOffsetDelta = xMinOffsetDelta + entry:getXLength() + 2 * entry.xMargin
+            end
+
+            mergeArrays(entryArray, oldEntries)
+            self.entries = entryArray
+            self.xMinOffset = self.xMinOffset - xMinOffsetDelta
+
+            return oldEntries
+        end,
+    }
+}
+
+-- Merges the second array into the first one.
+--
+-- Args:
+-- * array1: The modified Array.
+-- * array2: The Array to append to `array1`.
+--
+mergeArrays = function(array1, array2)
+    local count1 = array1.count
+    local count2 = array2.count
+    for i=1,count2 do
+        array1[count1+i] = array2[i]
+    end
+    array1.count = count1 +  count2
+end
+
+return LayerXPassBlock
