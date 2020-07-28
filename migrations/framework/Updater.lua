9db5a2bf2a491cd1c3ebd48d24ee68fbb3a0d637
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-07-28 09:37:53 +0200

    Migration: add a framework for migration scripts.

---
+++ migrations/framework/Updater.lua
@@ -0,0 +1,101 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local compareArrays
+local parseVersion
+
+-- Utility library for migration scripts.
+--
+local Updater = {
+    -- Runs a patch after performing version checks.
+    --
+    -- If installVersion is the current version in the save file:
+    -- * if targetVersion <= installVersion: the patch is skipped.
+    -- * if minSupportedVersion <= installVersion < targetVersion:
+    --       the patch is applied, and installVersion is set to targetVersion.
+    -- * if installVersion < minSupportedVersion: An error is thrown.
+    --
+    -- Args:
+    -- * minSupportedVersion: Minimal version required for this patch to be applied.
+    -- * targetVersion: Target version, obtained after application of this patch
+    -- * callback: Function to call to apply the patch.
+    --
+    run = function(minSupportedVersion, targetVersion, callback)
+        local installVersion = global.Dana.version or "0.1.0"
+        local installArray = parseVersion(installVersion)
+        local targetArray = parseVersion(targetVersion)
+        if compareArrays(installArray, targetArray) < 0 then
+            local minArray = parseVersion(minSupportedVersion)
+            if compareArrays(minArray, installArray) > 0 then
+                error("Migration from '" .. targetVersion .. "' failed. Minimum supported: '" .. minSupportedVersion .. "'. Installed: '" .. installVersion .. "'.")
+            end
+            callback()
+            global.Dana.version = targetVersion
+        else
+            log("Migration skipped (target: '" .. targetVersion .."' / installed: '" .. installVersion .. "').")
+        end
+    end,
+}
+
+-- Compares version arrays using lexicographic order.
+--
+-- Args:
+-- * v1: A version array.
+-- * v2: Another version array.
+--
+-- Returns: An integer (0 for equality, -1 if v1 < v2, 1 if v1 > v2).
+--
+compareArrays = function(v1, v2)
+    local l1,l2 = #v1,#v2
+    local minLen = math.min(l1,l2)
+    local result = 0
+
+    local i = 1
+    while result == 0 and i <= minLen do
+        local n1,n2 = v1[i],v2[i]
+        if n1 < n2 then
+            result = -1
+        elseif n1 > n2 then
+            result = 1
+        end
+        i = i + 1
+    end
+    if result == 0 then
+        if l1 < l2 then
+            result = -1
+        elseif l1 > l2 then
+            result = 1
+        end
+    end
+    return result
+end
+
+-- Parses a string representing a version into an array of integer.
+--
+-- Args:
+-- * strVersion: String representing the version (must be "a.b.c.d...", with only positive integers).
+--
+-- Returns: An array of integers {a,b,c,d,...}.
+--
+parseVersion = function(strVersion)
+    local result = {}
+    for subStr in string.gmatch(strVersion, "([^%.]+)") do
+        table.insert(result, tonumber(subStr))
+    end
+    return result
+end
+
+return Updater
---
+++ migrations/framework/tests/Updater.lua
@@ -0,0 +1,83 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local Updater = require("migrations/framework/Updater")
+
+-- Best logger mock ever.
+_G.log = function(str) end
+
+describe("Updater", function()
+    local flag
+
+    local flagSetter = function()
+        flag = true
+    end
+
+    before_each(function()
+        flag =  false
+        _G.global = {
+            Dana = {
+                version = "7.5.1",
+            },
+        }
+    end)
+
+    describe(".run(min,target)", function()
+        it("-- valid (min < Dana < target)", function()
+            Updater.run("7.5.0", "7.5.2", flagSetter)
+            assert.is_true(flag)
+            assert.are.equals(global.Dana.version, "7.5.2")
+        end)
+
+        it("-- valid (min == Dana < target)", function()
+            Updater.run("7.5.1", "7.5.11", flagSetter)
+            assert.is_true(flag)
+            assert.are.equals(global.Dana.version, "7.5.11")
+        end)
+
+        it("-- skip (min < Dana == target)", function()
+            Updater.run("7.4.0", "7.5.1", flagSetter)
+            assert.is_false(flag)
+            assert.are.equals(global.Dana.version, "7.5.1")
+        end)
+
+        it("-- skip (min < target < Dana)", function()
+            Updater.run("7.4.11", "7.5.0", flagSetter)
+            assert.is_false(flag)
+            assert.are.equals(global.Dana.version, "7.5.1")
+        end)
+
+        it("-- error (Dana < min)", function()
+            assert.error(function()
+                Updater.run("7.5.2", "11.12.13", flagSetter)
+            end)
+            assert.is_false(flag)
+            assert.are.equals(global.Dana.version, "7.5.1")
+        end)
+
+        it("-- skip (min < Dana < target) + variable length", function()
+            Updater.run("7.4", "7.5", flagSetter)
+            assert.is_false(flag)
+            assert.are.equals(global.Dana.version, "7.5.1")
+        end)
+
+        it("-- valid (min < Dana < target) + variable length", function()
+            Updater.run("7.5", "7.5.1.1", flagSetter)
+            assert.is_true(flag)
+            assert.are.equals(global.Dana.version, "7.5.1.1")
+        end)
+    end)
+end)
