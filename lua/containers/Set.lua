be58ecdbc54b1f7b8285d52ea0af6e848dfe2072
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-09-25 17:25:53 +0200

    Set: new pseudo-class for sets.

---
+++ lua/containers/Set.lua
@@ -0,0 +1,81 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+
+local checkCount
+
+-- Pseudo-class representing a genering set of objects.
+--
+-- Any table whose values are not "false" is considered a set.
+--
+local Set = ErrorOnInvalidRead.new{
+    -- Tests if two sets are equals.
+    --
+    -- This is a "set" equality test, not a "map" test: only keys are checked.
+    --
+    -- Args:
+    -- * set1: Set object.
+    -- * set2: Another Set.
+    --
+    -- Returns: True if the sets contain the same elements.
+    --
+    areEquals = function(set1, set2)
+        local result = true
+        local count = 0
+        for value in pairs(set1) do
+            result = result and rawget(set2, value)
+            count = count + 1
+        end
+        return (result and checkCount(set2, count)) or false
+    end,
+
+    -- Checks if a set has exactly the given amount of elements.
+    --
+    -- Args:
+    -- * set: Set object.
+    -- * expectedCount: Expected number of elements in `set`.
+    --
+    -- Returns: True if `set` has exactly `expectedCount` elements.
+    --
+    checkCount = function(set, expectedCount)
+        local _next = next
+        local value = nil
+        repeat
+            value = _next(set, value)
+            expectedCount = expectedCount - 1
+        until value == nil or expectedCount < -1
+        return expectedCount == -1
+    end,
+
+    -- Checks if a set is a singleton of the specified element.
+    --
+    -- Args:
+    -- * set: Set object.
+    -- * element: Expected element in `set`.
+    --
+    -- Returns: True if `set` contains only `element`. False otherwise.
+    --
+    checkSingleton = function(set, value)
+        local v1 = next(set)
+        local v2 = next(set, v1)
+        return (v1 == value) and (v2 == nil)
+    end,
+}
+
+checkCount = Set.checkCount
+
+return Set
---
+++ lua/containers/tests/Set.lua
@@ -0,0 +1,110 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local Set = require("lua/containers/Set")
+
+describe("Set",function()
+    describe(".areEquals()", function()
+        local runEqualTest = function(set1, set2, expectedResult)
+            assert.are.equals(Set.areEquals(set1, set2), expectedResult)
+            assert.are.equals(Set.areEquals(set2, set1), expectedResult)
+        end
+
+        it("-- true (both empty)", function()
+            runEqualTest({}, {}, true)
+        end)
+
+        it("-- false (one empty)", function()
+            runEqualTest({a = true}, {}, false)
+        end)
+
+        it("-- true (non-trivial)", function()
+            local set1 = {
+                a = true,
+                b = 0,
+                z = true,
+            }
+            local set2 = {
+                a = 1,
+                b = 2,
+                z = true,
+            }
+            runEqualTest(set1, set2, true)
+        end)
+
+        it("-- false (non-trivial)", function()
+            local set1 = {
+                a = true,
+                b = true,
+            }
+            local set2 = {
+                a = true,
+                b = true,
+                z = true,
+            }
+            runEqualTest(set1, set2, false)
+        end)
+    end)
+
+    describe(".checkCount()", function()
+        it("-- true (empty & 0)", function()
+            assert.is_true(Set.checkCount({}, 0))
+        end)
+
+        it("-- false (empty & 1)", function()
+            assert.is_false(Set.checkCount({}, 1))
+        end)
+
+        it("-- true (5)", function()
+            local set = {
+                the = true,
+                cake = true,
+                is = true,
+                a = true,
+                lie = true,
+            }
+            assert.is_true(Set.checkCount(set, 5))
+        end)
+
+        it("-- false (non-empty)", function()
+            local set = {
+                Kilroy = true,
+                was = true,
+                here = true,
+            }
+            assert.is_false(Set.checkCount(set, 2))
+            assert.is_false(Set.checkCount(set, 4))
+        end)
+    end)
+
+    describe(".checkSingleton()", function()
+        it("-- true", function()
+            assert.is_true(Set.checkSingleton({z = true}, "z"))
+        end)
+
+        it("-- false (wrong value)", function()
+            assert.is_false(Set.checkSingleton({z = true}, "a"))
+        end)
+
+        it("-- false (empty)", function()
+            assert.is_false(Set.checkSingleton({}, "z"))
+        end)
+
+        it("-- false (2 elements)", function()
+            assert.is_false(Set.checkSingleton({z = true, a = true}, "z"))
+        end)
+    end)
+end)
