f7f8c32485eed074a5a0c972b918ed5a57d6275f
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-09-30 20:52:14 +0200

    FullGraphQuery: override execute().

--- lua/model/query/FullGraphQuery.lua
+++ lua/model/query/FullGraphQuery.lua
@@ -16,8 +16,12 @@
 
 local AbstractQuery = require("lua/model/query/AbstractQuery")
 local AllQueryFilter = require("lua/model/query/filter/AllQueryFilter")
+local DirectedHypergraph = require("lua/hypergraph/DirectedHypergraph")
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+local QueryOrderer = require("lua/model/query/QueryOrderer")
+local QuerySelector = require("lua/model/query/QuerySelector")
 
+local Metatable
 local QueryType
 
 -- Query generating the full crafting graph.
@@ -28,14 +32,46 @@ local FullGraphQuery = ErrorOnInvalidRead.new{
     -- Returns: The new FullGraphQuery object.
     --
     new = function()
-        return AbstractQuery.new{
+        return AbstractQuery.new({
             filter = AllQueryFilter.new(),
             queryType = QueryType,
-        }
+        }, Metatable)
     end,
 
     -- Restores the metatable of a FullGraphQuery object, and all its owned objects.
-    setmetatable = AbstractQuery.setmetatable,
+    --
+    -- Args:
+    -- * object: table to modify.
+    --
+    setmetatable = function(object)
+        AbstractQuery.setmetatable(object, Metatable)
+    end,
+}
+
+-- Metatable of the FullGraphQuery class.
+Metatable = {
+    __index = ErrorOnInvalidRead.new{
+        -- Implements AbstractQuery:execute().
+        execute = function(self, force)
+            local selector = QuerySelector.new()
+            local fullGraph = selector:makeHypergraph(force)
+
+            local orderer = QueryOrderer.new()
+            local vertexDists = orderer:makeOrder(force, fullGraph)
+
+            local fullEdgeSet = {}
+            for _,edge in pairs(fullGraph.edges) do
+                fullEdgeSet[edge] = true
+            end
+            local filteredEdgeSet = self.filter:execute(fullEdgeSet)
+            local resultGraph = DirectedHypergraph.new()
+            for edge in pairs(filteredEdgeSet) do
+                resultGraph:addEdge(edge)
+            end
+
+            return resultGraph,vertexDists
+        end,
+    },
 }
 
 -- Identifier for this subtype of Query.
