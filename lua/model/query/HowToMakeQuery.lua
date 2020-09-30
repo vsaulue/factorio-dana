8c0964d0394b39b7f997af5afed063fa7a41d9c3
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-09-30 21:01:15 +0200

    HowToMakeQuery: override execute().

--- lua/model/query/HowToMakeQuery.lua
+++ lua/model/query/HowToMakeQuery.lua
@@ -15,9 +15,13 @@
 -- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
 
 local AbstractQuery = require("lua/model/query/AbstractQuery")
+local DirectedHypergraph = require("lua/hypergraph/DirectedHypergraph")
 local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
 local ReachableQueryFilter = require("lua/model/query/filter/ReachableQueryFilter")
+local QueryOrderer = require("lua/model/query/QueryOrderer")
+local QuerySelector = require("lua/model/query/QuerySelector")
 
+local Metatable
 local QueryType
 
 -- Query generating a subgraph showing how to craft some given intermediates.
@@ -28,16 +32,48 @@ local HowToMakeQuery = ErrorOnInvalidRead.new{
     -- Returns: The new HowToMakeQuery object.
     --
     new = function()
-        return AbstractQuery.new{
+        return AbstractQuery.new({
             filter = ReachableQueryFilter.new{
                 isForward = false,
             },
             queryType = QueryType,
-        }
+        }, Metatable)
     end,
 
     -- Restores the metatable of a HowToMakeQuery object, and all its owned objects.
-    setmetatable = AbstractQuery.setmetatable,
+    --
+    -- Args:
+    -- * object: table to modify.
+    --
+    setmetatable = function(object)
+        AbstractQuery.setmetatable(object, Metatable)
+    end,
+}
+
+-- Metatable of the HowToMakeQuery class.
+Metatable = {
+    __index = ErrorOnInvalidRead.new{
+        -- Implements AbstractQuery:execute().
+        execute = function(self, force)
+            local selector = QuerySelector.new()
+            local fullGraph = selector:makeHypergraph(force)
+
+            local orderer = QueryOrderer.new()
+            local vertexDists = orderer:makeOrder(force, fullGraph)
+
+            local fullEdgeSet = {}
+            for _,edge in pairs(fullGraph.edges) do
+                fullEdgeSet[edge] = true
+            end
+            local filteredEdgeSet = self.filter:execute(fullEdgeSet)
+            local resultGraph = DirectedHypergraph.new()
+            for edge in pairs(filteredEdgeSet) do
+                resultGraph:addEdge(edge)
+            end
+
+            return resultGraph,vertexDists
+        end,
+    },
 }
 
 -- Identifier for this subtype of AbstractQuery.
