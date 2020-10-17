ebe870d2848bf09444c1cee1c8d42ac7a8ef7f94
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-10-17 18:17:56 +0200

    MockObject: new base class for mocks.

---
+++ lua/testing/mocks/MockObject.lua
@@ -0,0 +1,101 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local MockMetatable = require("lua/testing/mocks/MockMetatable")
+
+local DataIndex
+local getClassLogger
+local Metatable
+
+-- Class providing extensive read/write access control for mocking purposes.
+--
+-- Fields are not directly stored in the object's table. They are instead stored
+-- in a "data" table (hidden at `DataIndex`). This enables interception of all read/write
+-- operations via __index/__newindex.
+--
+-- The default __index & __newindex of this class errors when accessing an unknown key.
+--
+local MockObject = {
+    -- Gets the ClassLogger object of the specified MockObject.
+    --
+    -- Args:
+    -- * self: MockObject.
+    --
+    -- Returns: The ClassLogger for the type of `self`.
+    --
+    getClassLogger = function(self)
+        return getmetatable(self).cLogger
+    end,
+
+    -- Gets the internal data table if the object is still valid.
+    --
+    -- Args:
+    -- * self: MockObject.
+    --
+    -- Returns: table or nil. The data table if the object is valid, nil otherwise.
+    --
+    getDataIfValid = function(self)
+        return rawget(self, DataIndex)
+    end,
+
+    -- Invalidates a MockObject.
+    --
+    -- This deletes the internal data table.
+    --
+    -- Args:
+    -- * self: MockObject.
+    --
+    invalidate = function(self)
+        self[DataIndex] = nil
+    end,
+
+    -- Creates a new MockObject.
+    --
+    -- Args:
+    -- * data (optional): table. Value to use as the internal data table.
+    -- * metatable (optional): Metatable to set to the new object.
+    --
+    -- Returns: MockObject. The newly construct object.
+    --
+    make = function(data, metatable)
+        local result = {
+            [DataIndex] = data or {},
+        }
+        setmetatable(result, metatable or Metatable)
+        return result
+    end,
+
+    -- Metatable of the MockObject class.
+    Metatable = MockMetatable.new{
+        className = "MockObject",
+
+        __index = function(self, index)
+            getClassLogger(self):error("Invalid read at '" .. tostring(index) .. "'.")
+        end,
+
+        __newindex = function(self, index)
+            getClassLogger(self):error("Invalid write at '" .. tostring(index) .. "'.")
+        end,
+    },
+}
+
+-- "Secret" index in which the data table is stored.
+DataIndex = {}
+
+getClassLogger = MockObject.getClassLogger
+Metatable = MockObject.Metatable
+
+return MockObject
---
+++ lua/testing/mocks/tests/MockObject.lua
@@ -0,0 +1,58 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local MockObject = require("lua/testing/mocks/MockObject")
+
+describe("MockObject", function()
+    local object
+
+    before_each(function()
+        object = MockObject.make()
+    end)
+
+    it(".make() -- custom data & metatable", function()
+        local m = {}
+        local d = {}
+        local o2 = MockObject.make(d, m)
+        assert.are.equals(MockObject.getDataIfValid(o2), d)
+        assert.are.equals(getmetatable(o2), m)
+    end)
+
+    it("-- invalid read", function()
+        assert.error(function()
+            print(object.foo)
+        end)
+    end)
+
+    it("-- invalid write", function()
+        assert.error(function()
+            object.bar = "foo"
+        end)
+    end)
+
+    describe(".getDataIfValid() & .invalidate()", function()
+        it("-- valid", function()
+            local data = MockObject.getDataIfValid(object)
+            assert.is_not_nil(data)
+        end)
+
+        it("-- invalidated", function()
+            MockObject.invalidate(object)
+            local data = MockObject.getDataIfValid(object)
+            assert.is_nil(data)
+        end)
+    end)
+end)
