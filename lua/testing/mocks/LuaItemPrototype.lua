9ff8ff496394bfbc25d19ce9a84590b7300655e1
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-10-17 18:26:28 +0200

    LuaItemPrototype: new mock.

---
+++ lua/testing/mocks/LuaItemPrototype.lua
@@ -0,0 +1,55 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local AbstractPrototype = require("lua/testing/mocks/AbstractPrototype")
+
+local checkType
+local cLogger
+local Metatable
+
+-- Mock implementation of Factorio's LuaItemPrototype.
+--
+-- See https://lua-api.factorio.com/1.0.0/LuaItemPrototype.html
+--
+-- Inherits from AbstractPrototype.
+--
+-- Implemented fields & methods:
+-- + AbstractPrototype.
+--
+local LuaItemPrototype = {
+    -- Creates a new LuaItemPrototype object.
+    --
+    -- Args:
+    -- * rawData: table. Construction argument from the data phase.
+    --
+    -- Returns: LuaItemPrototype. The new object.
+    --
+    make = function(rawData)
+        -- Note: too restrictive (there are valid subtypes).
+        cLogger:assert(rawData.type == "item", "Unsupported type: " .. rawData.type)
+        return AbstractPrototype.make(rawData, Metatable)
+    end,
+
+    -- Metatable of the LuaItemPrototype class.
+    Metatable = AbstractPrototype.Metatable:makeSubclass{
+        className = "LuaItemPrototype",
+    }
+}
+
+cLogger = LuaItemPrototype.Metatable.cLogger
+Metatable = LuaItemPrototype.Metatable
+
+return LuaItemPrototype
---
+++ lua/testing/mocks/tests/LuaItemPrototype.lua
@@ -0,0 +1,46 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local LuaItemPrototype = require("lua/testing/mocks/LuaItemPrototype")
+
+describe("LuaItemPrototype", function()
+    describe(".make()", function()
+        it("--valid", function()
+            local object = LuaItemPrototype.make{
+                type = "item",
+                name = "some-ore",
+            }
+            assert.are.equals(getmetatable(object), LuaItemPrototype.Metatable)
+        end)
+
+        it("--invalid type", function()
+            assert.error(function()
+                object = LuaItemPrototype.make{
+                    type = "nope",
+                    name = "some-ore"
+                }
+            end)
+        end)
+    end)
+
+    it("-- parent properties", function()
+        local object = LuaItemPrototype.make{
+            type = "item",
+            name = "some-ore"
+        }
+        assert.are.same(object.localised_name, {"item-name.some-ore"})
+    end)
+end)
