3d553ba6cf254f2db97ab78a5b5bc767c4b220ee
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-10-17 18:17:06 +0200

    MockMetatable: new class to generate metatables.

---
+++ lua/testing/mocks/MockMetatable.lua
@@ -0,0 +1,123 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local ClassLogger = require("lua/logger/ClassLogger")
+local MockMetatableParams = require("lua/testing/mocks/MockMetatableParams")
+
+local cLogger = ClassLogger.new{className = "MockMetatable"}
+
+local makeSubclassIndex
+local makeSubclassNewIndex
+local Metatable
+
+-- Class for metatables used in the Mock framework.
+--
+-- RO Fields:
+-- * autoLoaded: boolean. Flag set for SaveLoadTester (should be always true).
+-- * className: string. Name of this class (preferably unique, but not necessary).
+-- * cLogger: ClassLogger. Logger for the objects using this metatable.
+-- * __index: function.
+-- * __newindex: function.
+--
+local MockMetatable = {
+    -- Creates a new MockMetatable object.
+    --
+    -- Args:
+    -- * object: Table to turn into a MockMetatable object.
+    --
+    -- Returns: The argument turned into a MockMetatable object.
+    --
+    new = function(object)
+        local className = cLogger:assertFieldType(object, "className", "string")
+        cLogger:assertFieldType(object, "__index", "function")
+        cLogger:assertFieldType(object, "__newindex", "function")
+        object.autoLoaded = true
+        object.cLogger = ClassLogger.new{className = className}
+        setmetatable(object, Metatable)
+        return object
+    end,
+}
+
+-- Metatable of the MockMetatable class.
+Metatable = {
+    __index = {
+        -- Creates a new MockMetatable, for a derived type.
+        --
+        -- This will automatically chains the __index & __newindex calls.
+        --
+        -- Args:
+        -- * self: MockMetatable.
+        -- * params: MockMetatableParams. Parameters of the derived type.
+        --
+        -- Returns: MockMetatable. The new derived metatable.
+        --
+        makeSubclass = function(self, params)
+            MockMetatableParams.check(params)
+            return MockMetatable.new{
+                className = params.className,
+                __index = makeSubclassIndex(self, params),
+                __newindex = makeSubclassNewIndex(self, params),
+            }
+        end,
+    },
+}
+
+-- Creates the __index metamethod for a subclass.
+--
+-- Args:
+-- * base: MockMetatable. Metatable to use as parent.
+-- * params: MockMetatableParams. Parameters of the derived type.
+--
+-- Returns: function(*,*) -> *. The __index function for the subclass.
+--
+makeSubclassIndex = function(base, params)
+    local baseIndex = base.__index
+    local getters = params.getters
+    return function(self, index)
+        if getters then
+            local getter = getters[index]
+            if getter then
+                return getter(self)
+            end
+        end
+        return baseIndex(self, index)
+    end
+end
+
+-- Creates the __newindex metamethod for a subclass.
+--
+-- Args:
+-- * base: MockMetatable. Metatable to use as parent.
+-- * params: MockMetatableParams. Parameters of the derived type.
+--
+-- Returns: function(*,*,*) -> *. The __newindex function for the subclass.
+--
+makeSubclassNewIndex = function(base, params)
+    local baseNewIndex = base.__newindex
+    local setters = params.setters
+    return function(self, index, value)
+        if setters then
+            local setter = setters[index]
+            if setter then
+                setter(self, value)
+                return
+            end
+        end
+        baseNewIndex(self, index, value)
+    end
+end
+
+return MockMetatable
---
+++ lua/testing/mocks/tests/MockMetatable.lua
@@ -0,0 +1,83 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local MockMetatable = require("lua/testing/mocks/MockMetatable")
+
+describe("MockMetatable", function()
+    it(".new() -- valid", function()
+        local metatable = MockMetatable.new{
+            className = "foobar",
+            __index = function() end,
+            __newindex = function() end,
+        }
+        assert.is_true(metatable.autoLoaded)
+        assert.are.equals(metatable.cLogger.className, metatable.className)
+        assert.is_not_nil(metatable.cLogger.error)
+    end)
+
+    describe(":makeSubclass()", function()
+        local BaseMetatable
+        local DerivedMetatable
+        local derived
+
+        before_each(function()
+            BaseMetatable = MockMetatable.new{
+                className = "base",
+                __index = function(self, index)
+                    return {self, "base", index}
+                end,
+                __newindex = function(self, index, value)
+                    rawset(self, index, {self, "base", value})
+                end,
+            }
+            DerivedMetatable = BaseMetatable:makeSubclass{
+                className = "derived",
+                getters = {
+                    foobar = function(self)
+                        return {self, "derived", "foo"}
+                    end,
+                },
+                setters = {
+                    barfoo = function(self, value)
+                        rawset(self, "barfoo", {self, "derived", value})
+                    end,
+                },
+            }
+            derived = {}
+            setmetatable(derived, DerivedMetatable)
+        end)
+
+        it("-- autoLoaded", function()
+            assert.is_true(DerivedMetatable.autoLoaded)
+        end)
+
+        it("-- className", function()
+            assert.are.equals(DerivedMetatable.className, "derived")
+        end)
+
+        it("-- __index", function()
+            assert.are.same(derived.kilroy, {derived, "base", "kilroy"})
+            assert.are.same(derived.foobar, {derived, "derived", "foo"})
+        end)
+
+        it("-- __setindex", function()
+            derived.kilroy = "was here"
+            assert.are.same(rawget(derived, "kilroy"), {derived, "base", "was here"})
+            derived.barfoo = "wololo"
+            assert.are.same(rawget(derived, "barfoo"), {derived, "derived", "wololo"})
+        end)
+    end)
+end)
