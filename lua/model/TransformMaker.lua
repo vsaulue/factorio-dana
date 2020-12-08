c94087441bfb984f9b54f3fd2c59398d02f70123
Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
2020-12-08 18:42:29 +0100

    TransformMaker: new class to build transforms.

---
+++ lua/model/TransformMaker.lua
@@ -0,0 +1,166 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local ClassLogger = require("lua/logger/ClassLogger")
+local ErrorOnInvalidRead = require("lua/containers/ErrorOnInvalidRead")
+local FlyweightFactory = require("lua/class/FlyweightFactory")
+local ProductAmount = require("lua/model/ProductAmount")
+local ProductData = require("lua/model/ProductData")
+
+local cLogger = ClassLogger.new{className = "TransformMaker"}
+
+local addProduct
+local Metatable
+
+-- Utility class to build AbstractTransform objects.
+--
+-- RO Fields:
+-- * intermediates: IntermediatesDatabase. Intermediates to use as ingredients or products.
+-- * transform (optional): table. Future constructor argument for an AbstractTransform.
+-- **  ingredients: table.
+-- **  products: table.
+--
+local TransformMaker = ErrorOnInvalidRead.new{
+    new = function(object)
+        cLogger:assertField(object, "intermediates")
+        object.productAmountFactory = FlyweightFactory.new{
+            make = ProductAmount.copy,
+            valueEquals = ProductAmount.equals,
+        }
+        setmetatable(object, Metatable)
+        return object
+    end,
+}
+
+Metatable = {
+    __index = ErrorOnInvalidRead.new{
+        -- Adds a product to the transform.
+        --
+        -- Args:
+        -- * self: TransformMaker.
+        -- * iType: string. Type of the added Intermediate.
+        -- * name: string. Name of the added Intermediate.
+        -- * productAmount: Associated ProductAmount object.
+        --
+        addConstantProduct = function(self, iType, name, count)
+            addProduct(self, iType, name, {
+                amountMax = count,
+                amountMin = count,
+                probability = 1,
+            })
+        end,
+
+        -- Adds an ingredient to the transform.
+        --
+        -- Args:
+        -- * self: TransformMaker.
+        -- * iType: string. Type of the added Intermediate.
+        -- * name: string. Name of the added Intermediate.
+        -- * amount: int. Amount consumed by the transform.
+        --
+        addIngredient = function(self, iType, name, amount)
+            local intermediate = self.intermediates[iType][name]
+            local ingredients = self.transform.ingredients
+            local oldQuantity = (rawget(ingredients, intermediate) or 0)
+            ingredients[intermediate] = oldQuantity + amount
+        end,
+
+        -- Adds an array of Ingredient from the API.
+        --
+        -- Args:
+        -- * self: TransformMaker.
+        -- * rawIngredients: Array<Ingredient>. Ingredients from Factorio's API.
+        --
+        addRawIngredientArray = function(self, rawIngredients)
+            for _,rawIngredient in ipairs(rawIngredients) do
+                self:addIngredient(rawIngredient.type, rawIngredient.name, rawIngredient.amount)
+            end
+        end,
+
+        -- Adds an array of Ingredient from the API.
+        --
+        -- Args:
+        -- * self: TransformMaker.
+        -- * rawProduct: Product. Product from Factorio's API.
+        --
+        addRawProduct = function(self, rawProduct)
+            local amountMax = rawProduct.amount
+            local amountMin
+            if amountMax then
+                amountMin = amountMax
+            else
+                amountMax = rawProduct.amount_max
+                amountMin = rawProduct.amount_min
+            end
+            addProduct(self, rawProduct.type, rawProduct.name, {
+                amountMax = amountMax,
+                amountMin = amountMin,
+                probability = rawProduct.probability or 1,
+            })
+        end,
+
+        -- Adds an array of Ingredient from the API.
+        --
+        -- Args:
+        -- * self: TransformMaker.
+        -- * rawIngredients: Array<Ingredient>. Ingredients from Factorio's API.
+        --
+        addRawProductArray = function(self, rawProductArray)
+            for _,rawProduct in ipairs(rawProductArray) do
+                self:addRawProduct(rawProduct)
+            end
+        end,
+
+        -- Resets the transform field, to be used to generate a new transform.
+        --
+        -- Args:
+        -- * self: TransformMaker.
+        -- * object (optional): table. Table to use as the new transform.
+        --
+        newTransform = function(self, object)
+            local transform = object or {}
+            transform.products = {}
+            transform.ingredients = {}
+            self.transform = transform
+            return transform
+        end,
+    },
+}
+
+-- Adds a product to the transform.
+--
+-- Args:
+-- * self: TransformMaker.
+-- * iType: string. Type of the added Intermediate.
+-- * name: string. Name of the added Intermediate.
+-- * productAmount: Associated ProductAmount object.
+--
+addProduct = function(self, iType, name, rawAmount)
+    if rawAmount.amountMax > 0 and rawAmount.probability > 0 then
+        local amount = self.productAmountFactory:get(rawAmount)
+        local products = self.transform.products
+        local intermediate = self.intermediates[iType][name]
+        local productData = rawget(products, intermediate)
+        if not productData then
+            productData = ProductData.make(amount)
+            products[intermediate] = productData
+        else
+            productData:addAmount(amount)
+        end
+    end
+end
+
+return TransformMaker
---
+++ lua/model/tests/TransformMaker.lua
@@ -0,0 +1,122 @@
+-- This file is part of Dana.
+-- Copyright (C) 2020 Vincent Saulue-Laborde <vincent_saulue@hotmail.fr>
+--
+-- Dana is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- Dana is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with Dana.  If not, see <https://www.gnu.org/licenses/>.
+
+local IntermediatesDatabase = require("lua/model/IntermediatesDatabase")
+local MockFactorio = require("lua/testing/mocks/MockFactorio")
+local TransformMaker = require("lua/model/TransformMaker")
+
+describe("TransformMaker", function()
+    local factorio
+    local intermediates
+    setup(function()
+        factorio = MockFactorio.make{
+            rawData = {
+                fluid = {
+                    steam = {type = "fluid", name = "steam"},
+                    water = {type = "fluid", name = "water"},
+                },
+                item = {
+                    wood = {type = "item", name = "wood"},
+                    coal ={type = "item", name = "coal"},
+                },
+            },
+        }
+        intermediates = IntermediatesDatabase.new()
+        intermediates:rebuild(factorio.game)
+    end)
+
+    local context
+    before_each(function()
+        context = TransformMaker.new{
+            intermediates = intermediates,
+        }
+    end)
+
+    it(":addConstantProduct()", function()
+        context:newTransform()
+        context:addConstantProduct("item", "coal", 5)
+        assert.are.same(context.transform.products, {
+            [intermediates.item.coal] = {
+                count = 1,
+                [1] = {amountMax = 5, amountMin = 5, probability = 1},
+            },
+        })
+    end)
+
+    it(":addIngredient()", function()
+        context:newTransform()
+        context:addIngredient("fluid", "water", 8)
+        assert.are.same(context.transform.ingredients, {
+            [intermediates.fluid.water] = 8,
+        })
+    end)
+
+    it(":addRawIngredientArray", function()
+        context:newTransform()
+        context:addRawIngredientArray{
+            {type = "item", name = "wood", amount = 5},
+            {type = "fluid", name = "water", amount = 3},
+            {type = "item", name = "wood", amount = 2},
+        }
+        assert.are.same(context.transform.ingredients, {
+            [intermediates.item.wood] = 7,
+            [intermediates.fluid.water] = 3,
+        })
+    end)
+
+    it(":addRawProduct", function()
+        context:newTransform()
+        context:addRawProduct({
+            type = "fluid",
+            name = "water",
+            amount_max = 5,
+            amount_min = 3,
+            probability = 0.25,
+        })
+        assert.are.same(context.transform.products, {
+            [intermediates.fluid.water] = {
+                count = 1,
+                [1] = {amountMax = 5, amountMin = 3, probability = 0.25},
+            },
+        })
+    end)
+
+    it(":addRawProductArray()", function()
+        context:newTransform{}
+        context:addRawProductArray{
+            {type = "item", name = "coal", amount = 1},
+            {type = "fluid", name = "water", amount_max = 2, amount_min = 1, probability = 0.5},
+            {type = "item", name = "coal", amount = 2},
+        }
+        assert.are.same(context.transform.products, {
+            [intermediates.item.coal] = {
+                count = 2,
+                [1] = {amountMax = 1, amountMin = 1, probability = 1},
+                [2] = {amountMax = 2, amountMin = 2, probability = 1},
+            },
+            [intermediates.fluid.water] = {
+                count = 1,
+                [1] = {amountMax = 2, amountMin = 1, probability = 0.5},
+            },
+        })
+    end)
+
+    it(":newTransform()", function()
+        local result = context:newTransform()
+        assert.is_not_nil(result)
+        assert.are.equals(result, context.transform)
+    end)
+end)
